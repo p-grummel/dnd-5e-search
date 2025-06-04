414c67231f0acebf3e8d95ed9b62171420a409cd
p-grummel <philipp.grummel@gmail.com>
2025-06-04 19:15:56 +0200

    changed action names to not include numbers (this is not allowed)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # dnd-5e-search
 
 Commands:
-    - (TODO): 5eMenu -> Opens Menu with ability to select other options
-    - "5eMonsterManual" -> Opens search for Monsters
-    - (TODO): "5eSpellBook" -> Opens search for Spells
-    - (TODO): "5eItemList" -> Opens search for Items
+    - (TODO): DnDMenu -> Opens Menu with ability to select other options
+    - "DnDMonsterManual" -> Opens search for Monsters
+    - (TODO): "DnDSpellBook" -> Opens search for Spells
+    - (TODO): "DnDItemList" -> Opens search for Items
--- lua/dnd-5e-search.lua
+++ lua/dnd-5e-search.lua
@@ -4,28 +4,28 @@ local conf = require("telescope.config").values
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
-vim.api.nvim_create_user_command("5eMonsterManual", function()
-  pickers
-      .new({}, {
-        prompt_title = "Hello from Telescope",
-        finder = finders.new_table({
-          results = {
-            "Test1",
-            "Test2",
-            "Test3",
-            "Test4",
-            "Test5",
-          },
-        }),
-        sorter = conf.generic_sorter({}),
-        attach_mappings = function(prompt_bufnr, map)
-          actions.select_default:replace(function()
-            actions.close(prompt_bufnr)
-            local selection = action_state.get_selected_entry()
-            -- print("You selected: " .. selection[1])
-          end)
-          return true
-        end,
-      })
-      :find()
+vim.api.nvim_create_user_command("DnDMonsterManual", function()
+	pickers
+		.new({}, {
+			prompt_title = "Hello from Telescope",
+			finder = finders.new_table({
+				results = {
+					"Test1",
+					"Test2",
+					"Test3",
+					"Test4",
+					"Test5",
+				},
+			}),
+			sorter = conf.generic_sorter({}),
+			attach_mappings = function(prompt_bufnr, map)
+				actions.select_default:replace(function()
+					actions.close(prompt_bufnr)
+					local selection = action_state.get_selected_entry()
+					-- print("You selected: " .. selection[1])
+				end)
+				return true
+			end,
+		})
+		:find()
 end, {})
