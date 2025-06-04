1c8ffee40111c4c29996569fc2087777528ba0a1
p-grummel <philipp.grummel@gmail.com>
2025-06-04 19:12:50 +0200

    basic setup, open telescope buffer with prompt and hardcoded results

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dnd-5e-search
+
+Commands:
+    - (TODO): 5eMenu -> Opens Menu with ability to select other options
+    - "5eMonsterManual" -> Opens search for Monsters
+    - (TODO): "5eSpellBook" -> Opens search for Spells
+    - (TODO): "5eItemList" -> Opens search for Items
---
+++ lua/dnd-5e-search.lua
@@ -0,0 +1,31 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+vim.api.nvim_create_user_command("5eMonsterManual", function()
+  pickers
+      .new({}, {
+        prompt_title = "Hello from Telescope",
+        finder = finders.new_table({
+          results = {
+            "Test1",
+            "Test2",
+            "Test3",
+            "Test4",
+            "Test5",
+          },
+        }),
+        sorter = conf.generic_sorter({}),
+        attach_mappings = function(prompt_bufnr, map)
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            -- print("You selected: " .. selection[1])
+          end)
+          return true
+        end,
+      })
+      :find()
+end, {})
