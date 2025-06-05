3616e3032a6e08c5215b6d6adbb711a106be20b1
p-grummel <philipp.grummel@gmail.com>
2025-06-05 17:16:10 +0200

    send request for specific monster when selecting it. Does not work yet, request returns nil for some reason. Need to fix

--- lua/api_request.lua
+++ lua/api_request.lua
@@ -2,25 +2,27 @@ local Job = require("plenary.job")
 local constants = require("constants")
 
 local function sort_by_name(entry_1, entry_2)
-	return entry_1.name < entry_2.name
+  return entry_1.name < entry_2.name
 end
 
 return {
-	send = function(sort, prompt, callback)
-		Job:new({
-			command = "curl",
-			args = { "--silent", constants.BASE_URL .. prompt },
-			on_exit = function(j, return_val)
-				--TODO: Error handling
-				local output_string = table.concat(j:result(), "\n")
-				vim.schedule(function()
-					local json = vim.json.decode(output_string).results
-					if sort then
-						table.sort(json, sort_by_name)
-					end
-					callback(json)
-				end)
-			end,
-		}):start()
-	end,
+  send = function(sort, prompt, callback)
+    print(constants.BASE_URL .. prompt)
+    Job:new({
+      command = "curl",
+      args = { "--silent", constants.BASE_URL .. prompt },
+      on_exit = function(j, return_val)
+        --TODO: Error handling
+        local output_string = table.concat(j:result(), "\n")
+        vim.schedule(function()
+          local json = vim.json.decode(output_string).results
+          if sort then
+            table.sort(json, sort_by_name)
+          end
+          print(vim.inspect(json))
+          callback(json)
+        end)
+      end,
+    }):start()
+  end,
 }
--- lua/telescope_buffer.lua
+++ lua/telescope_buffer.lua
@@ -6,37 +6,37 @@ local action_state = require("telescope.actions.state")
 local constants = require("constants")
 
 return {
-  draw = function(table_content)
-    pickers
-        .new({}, {
-          prompt_title = constants.MONSTER_MANUAL_ICON .. "Monster Manual",
-          --INFO: entries shown at once are limited to 200 or so by default
-          -- -> maybe see how to change this in case it becomes annoying, but
-          -- seems fine for now
-          finder = finders.new_table({
-            results = table_content,
-            entry_maker = function(entry)
-              return {
-                value = entry.index,
-                ordinal = entry.name,
-                display = entry.name,
-                url = entry.url,
-              }
-            end,
-          }),
-          sorter = conf.generic_sorter({}),
-          sorting_strategy = "ascending",
-          scroll_strategy = "limit",
-          attach_mappings = function(prompt_bufnr, map)
-            actions.select_default:replace(function()
-              actions.close(prompt_bufnr)
-              local selection = action_state.get_selected_entry()
-              -- custom_floating_buffer.open_floating_window({ "Test", "Test2", "Test3" })
-              --TODO: Callback
-            end)
-            return true
-          end,
-        })
-        :find()
-  end,
+	draw = function(table_content, callback)
+		pickers
+			.new({}, {
+				prompt_title = constants.MONSTER_MANUAL_ICON .. "Monster Manual",
+				--INFO: entries shown at once are limited to 200 or so by default
+				-- -> maybe see how to change this in case it becomes annoying, but
+				-- seems fine for now
+				finder = finders.new_table({
+					results = table_content,
+					entry_maker = function(entry)
+						return {
+							value = entry.index,
+							ordinal = entry.name,
+							display = entry.name,
+							url = entry.url,
+						}
+					end,
+				}),
+				sorter = conf.generic_sorter({}),
+				sorting_strategy = "ascending",
+				scroll_strategy = "limit",
+				attach_mappings = function(prompt_bufnr, map)
+					actions.select_default:replace(function()
+						actions.close(prompt_bufnr)
+						local selection = action_state.get_selected_entry()
+						-- custom_floating_buffer.open_floating_window({ "Test", "Test2", "Test3" })
+						callback(selection)
+					end)
+					return true
+				end,
+			})
+			:find()
+	end,
 }
