9fd6a2074f20bafa4612ca4e43ae804ea2d58c14
p-grummel <philipp.grummel@gmail.com>
2025-06-05 12:38:42 +0200

    open rudimentary window which I will use later to display monster stats. Closes with escape

--- lua/dnd-5e-search.lua
+++ lua/dnd-5e-search.lua
@@ -5,6 +5,45 @@ local Job = require("plenary.job")
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
+local function open_floating_window(lines)
+  local buf = vim.api.nvim_create_buf(false, true) -- unlisted, scratch
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+
+  local width = math.floor(vim.o.columns * 0.8)
+  local height = math.floor(vim.o.lines * 0.6)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+
+  local opts = {
+    style = "minimal",
+    relative = "editor",
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    border = "rounded",
+  }
+
+  local win = vim.api.nvim_open_win(buf, true, opts)
+
+  -- Match Telescope-like style
+  vim.api.nvim_win_set_option(win, "winhighlight", "Normal:NormalFloat,FloatBorder:TelescopeBorder")
+
+  -- Keymap to close on Escape
+  vim.api.nvim_buf_set_keymap(buf, "n", "<Esc>", "", {
+    nowait = true,
+    noremap = true,
+    silent = true,
+    callback = function()
+      if vim.api.nvim_win_is_valid(win) then
+        vim.api.nvim_win_close(win, true)
+      end
+    end,
+  })
+
+  return buf, win
+end
+
 local function window_2(table_content)
   pickers
       .new({}, {
@@ -24,7 +63,7 @@ local function window_2(table_content)
           actions.select_default:replace(function()
             actions.close(prompt_bufnr)
             local selection = action_state.get_selected_entry()
-            api_request(selection[1])
+            open_floating_window({ "Test", "Test2", "Test3" })
           end)
           return true
         end,
