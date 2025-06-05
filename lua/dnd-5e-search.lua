167e2bd9dcd3b8946a648ab66ac6aa931fea3248
p-grummel <philipp.grummel@gmail.com>
2025-06-05 11:57:54 +0200

    close telescope on select and print selection

--- lua/dnd-5e-search.lua
+++ lua/dnd-5e-search.lua
@@ -2,38 +2,45 @@ local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local conf = require("telescope.config").values
 local Job = require("plenary.job")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 
 local function api_request(prompt)
-	Job:new({
-		command = "curl",
-		args = { "--silent", "https://www.dnd5eapi.co/api/2014/monsters/?name=" .. prompt },
-		on_exit = function(j, return_val)
-			local output = table.concat(j:result(), "\n")
-			vim.schedule(function()
-				print("API response:\n" .. output)
-			end)
-		end,
-	}):start()
+  Job:new({
+    command = "curl",
+    args = { "--silent", "https://www.dnd5eapi.co/api/2014/monsters/?name=" .. prompt },
+    on_exit = function(j, return_val)
+      local output = table.concat(j:result(), "\n")
+      vim.schedule(function()
+        print("API response:\n" .. output)
+      end)
+    end,
+  }):start()
 end
 
 local function custom_finder()
-	pickers
-		.new({}, {
-			prompt_title = "colors",
-			finder = finders.new_job(function(prompt)
-				print(prompt)
-				if prompt == nil or prompt == "" then
-					return nil
-				end
-				return { "echo", prompt }
-			end),
-			sorter = conf.generic_sorter({}),
-		})
-		:find()
+  pickers
+      .new({}, {
+        prompt_title = "test",
+        finder = finders.new_job(function(prompt)
+          return { "echo", prompt }
+        end),
+        sorter = conf.generic_sorter({}),
+        attach_mappings = function(prompt_bufnr, map)
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            print(selection[1])
+          end)
+          return true
+        end,
+      })
+      :find()
 end
 
 vim.api.nvim_create_user_command("DnDMonsterManual", function()
-	api_request("goblin")
+  -- api_request("goblin")
+  custom_finder()
 end, {})
 
 -- command = "curl",
