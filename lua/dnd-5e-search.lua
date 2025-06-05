6f5d73f5e15efe2e266c221b29a93d520ccb7a63
p-grummel <philipp.grummel@gmail.com>
2025-06-05 10:21:59 +0200

    rolled back on previous progress. Did not work and got confused. This might be a simpler starting point

--- lua/dnd-5e-search.lua
+++ lua/dnd-5e-search.lua
@@ -1,63 +1,26 @@
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local conf = require("telescope.config").values
-local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
 
-local Job = require("plenary.job")
-
-local function fetch_monsters_async(query, on_result)
-	if query == "" then
-		on_result({})
-		return
-	end
-
-	Job:new({
-		command = "curl",
-		args = {
-			"--silent",
-			"https://www.dnd5eapi.co/api/2014/monsters/?name=" .. query,
-		},
-		on_exit = function(j, return_val)
-			local raw = table.concat(j:result(), "\n")
-			local decoded = vim.fn.json_decode(raw) or {}
-			local results = {}
-			if decoded.results then
-				for _, monster in ipairs(decoded.results) do
-					table.insert(results, monster.name)
-				end
-			end
-			vim.schedule(function()
-				on_result(results)
-			end)
-		end,
-	}):start()
-end
-
-local function search_monsters()
+local function custom_finder()
 	pickers
 		.new({}, {
-			prompt_title = "Search DnD Monsters",
-			finder = finders.new_dynamic({
-				fn = function(prompt, process_result)
-					fetch_monsters_async(prompt, function(results)
-						process_result(results)
-					end)
-				end,
-			}),
+			prompt_title = "colors",
+			finder = finders.new_job(function(prompt)
+				print(prompt)
+				if prompt == nil or prompt == "" then
+					return nil
+				end
+				return { "echo", prompt }
+			end),
 			sorter = conf.generic_sorter({}),
-			attach_mappings = function(prompt_bufnr, map)
-				actions.select_default:replace(function()
-					local selection = action_state.get_selected_entry()
-					print("Selected: " .. selection[1])
-					actions.close(prompt_bufnr)
-				end)
-				return true
-			end,
 		})
 		:find()
 end
 
 vim.api.nvim_create_user_command("DnDMonsterManual", function()
-	search_monsters()
+	custom_finder()
 end, {})
+
+-- command = "curl",
+-- args = { "--silent", "https://dnd5eapi.co/api/2014/monsters/?name=" .. prompt },
