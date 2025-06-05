c99b643caffc627d0fa8c76090b1ed80a77ee716
p-grummel <philipp.grummel@gmail.com>
2025-06-05 11:41:35 +0200

    working http request, not built into telescope

--- lua/dnd-5e-search.lua
+++ lua/dnd-5e-search.lua
@@ -1,6 +1,20 @@
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local conf = require("telescope.config").values
+local Job = require("plenary.job")
+
+local function api_request(prompt)
+	Job:new({
+		command = "curl",
+		args = { "--silent", "https://www.dnd5eapi.co/api/2014/monsters/?name=" .. prompt },
+		on_exit = function(j, return_val)
+			local output = table.concat(j:result(), "\n")
+			vim.schedule(function()
+				print("API response:\n" .. output)
+			end)
+		end,
+	}):start()
+end
 
 local function custom_finder()
 	pickers
@@ -19,8 +33,8 @@ local function custom_finder()
 end
 
 vim.api.nvim_create_user_command("DnDMonsterManual", function()
-	custom_finder()
+	api_request("goblin")
 end, {})
 
 -- command = "curl",
--- args = { "--silent", "https://dnd5eapi.co/api/2014/monsters/?name=" .. prompt },
+-- args = { "--silent", "https://www.dnd5eapi.co/api/2014/monsters/?name=" .. prompt },
