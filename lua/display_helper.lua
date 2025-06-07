9daee47447029692c9b29526e31c425fc5f614e1
Philipp <philipp.grummel@gmail.com>
2025-06-07 12:20:45 +0200

    progress on armor class section

--- lua/display_helper.lua
+++ lua/display_helper.lua
@@ -1,22 +1,36 @@
 return {
 
-	display_monster = function(entry)
-		return {
-			-- Title
-			"___.:"
-				.. entry.name
-				.. ":.___",
-			"",
-			entry.size
-				.. " "
-				.. entry.type
-				.. (entry.subtype and " (" .. entry.subtype .. ")" or "")
-				.. ", "
-				.. entry.alignment,
-			"___",
-			"",
-			-- AC, HP, Speed
-			"**Armor Class**: " .. entry.armor_class[1].value,
-		}
-	end,
+  display_monster = function(entry)
+    local armor_piece_string = function()
+      if entry.armor_class[1].armor then
+        local t = {}
+        for _, i in ipairs(entry.armor_class[1].armor) do
+          table.insert(t, i.name)
+        end
+        return " (" .. table.concat(t) .. ")"
+      else
+        return ""
+      end
+    end
+
+    return {
+      -- Title
+      "___.:"
+      .. entry.name
+      .. ":.___",
+      "",
+      entry.size
+      .. " "
+      .. entry.type
+      .. (entry.subtype and " (" .. entry.subtype .. ")" or "")
+      .. ", "
+      .. entry.alignment,
+      "___",
+      "",
+      -- AC, HP, Speed
+      "**Armor Class**: "
+      .. entry.armor_class[1].value
+      .. armor_piece_string(),
+    }
+  end,
 }
