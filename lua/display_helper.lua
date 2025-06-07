03e03e5dc8a83ef1272a15c2f5d4d290f3961211
Philipp <philipp.grummel@gmail.com>
2025-06-07 14:06:24 +0200

    correctly format armor pieces string for monsters

--- lua/display_helper.lua
+++ lua/display_helper.lua
@@ -1,36 +1,36 @@
 return {
 
-  display_monster = function(entry)
-    local armor_piece_string = function()
-      if entry.armor_class[1].armor then
-        local t = {}
-        for _, i in ipairs(entry.armor_class[1].armor) do
-          table.insert(t, i.name)
-        end
-        return " (" .. table.concat(t) .. ")"
-      else
-        return ""
-      end
-    end
+	display_monster = function(entry)
+		local armor_pieces_string = function()
+			if entry.armor_class[1].armor then
+				local t = {}
+				for index, i in ipairs(entry.armor_class[1].armor) do
+					table.insert(t, (index == 1 and i.name or ", " .. i.name))
+				end
+				return " (" .. table.concat(t) .. ")"
+			else
+				return ""
+			end
+		end
 
-    return {
-      -- Title
-      "___.:"
-      .. entry.name
-      .. ":.___",
-      "",
-      entry.size
-      .. " "
-      .. entry.type
-      .. (entry.subtype and " (" .. entry.subtype .. ")" or "")
-      .. ", "
-      .. entry.alignment,
-      "___",
-      "",
-      -- AC, HP, Speed
-      "**Armor Class**: "
-      .. entry.armor_class[1].value
-      .. armor_piece_string(),
-    }
-  end,
+		return {
+			-- Title
+			"___.:"
+				.. entry.name
+				.. ":.___",
+			"",
+			entry.size
+				.. " "
+				.. entry.type
+				.. (entry.subtype and " (" .. entry.subtype .. ")" or "")
+				.. ", "
+				.. entry.alignment,
+			"___",
+			"",
+			-- AC, HP, Speed
+			"**Armor Class**: "
+				.. entry.armor_class[1].value
+				.. armor_pieces_string(),
+		}
+	end,
 }
