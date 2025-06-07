83e2bd35f7c4071ed42145be6d599f07ce13a57c
Philipp <philipp.grummel@gmail.com>
2025-06-07 14:44:55 +0200

    added speed values

--- lua/display_helper.lua
+++ lua/display_helper.lua
@@ -2,47 +2,54 @@ local utils = require("utils")
 
 return {
 
-	display_monster = function(entry)
-		local title_section = function()
-			return {
-				"___.:" .. entry.name .. ":.___",
-				"",
-				entry.size
-					.. " "
-					.. entry.type
-					.. (entry.subtype and " (" .. entry.subtype .. ")" or "")
-					.. ", "
-					.. entry.alignment,
-				"___",
-			}
-		end
+  display_monster = function(entry)
+    local title_section = function()
+      return {
+        "___.:" .. entry.name .. ":.___",
+        "",
+        entry.size
+        .. " "
+        .. entry.type
+        .. (entry.subtype and " (" .. entry.subtype .. ")" or "")
+        .. ", "
+        .. entry.alignment,
+        "___",
+      }
+    end
 
-		local ac_speed_hp_section = function()
-			local armor_pieces_string = function()
-				if entry.armor_class[1].armor then
-					local t = {}
-					for index, i in ipairs(entry.armor_class[1].armor) do
-						table.insert(t, (index == 1 and i.name or ", " .. i.name))
-					end
-					return " *(" .. table.concat(t) .. ")*"
-				else
-					return ""
-				end
-			end
+    local ac_speed_hp_section = function()
+      local armor_pieces_string = function()
+        if entry.armor_class[1].armor then
+          local t = {}
+          for index, i in ipairs(entry.armor_class[1].armor) do
+            table.insert(t, (index == 1 and i.name or ", " .. i.name))
+          end
+          return " *(" .. table.concat(t) .. ")*"
+        else
+          return ""
+        end
+      end
 
-			local speed_types = function()
-				for k, v in pairs(entry.speed) do
-				end
-			end
+      local speed_types = function()
+        local t = {}
+        for k, v in pairs(entry.speed) do
+          table.insert(t, "- " .. k .. ": " .. v)
+        end
+        return t
+      end
 
-			return {
-				"**Armor Class**: " .. entry.armor_class[1].value .. armor_pieces_string(),
-				"**HP**: " .. entry.hit_points,
-				-- "**Speed**: " .. entry.
-				"___",
-			}
-		end
+      return utils.merge_tables({
+        {
+          "**Armor Class**: " .. entry.armor_class[1].value .. armor_pieces_string(),
+          "**HP**: " .. entry.hit_points,
+          "___",
+          "**Speed**: ",
+        },
+        speed_types(),
+        { "___" },
+      })
+    end
 
-		return utils.merge_tables({ title_section(), ac_speed_hp_section() })
-	end,
+    return utils.merge_tables({ title_section(), ac_speed_hp_section() })
+  end,
 }
